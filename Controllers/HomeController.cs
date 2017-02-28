627d5818f8227c4b6958c9c76a9d19fcdb983290
dragonfly201270 <adi.3183@gmail.com>
2017-02-28 16:09:55 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,17 +18,17 @@ namespace WebCache.Controllers
            _cache = cache;
           }
        public IActionResult Index ()
-{
-string value = _cache.GetString ("Time");
-if (value == null)
-{
-DateTime.Now.ToString value = ();
-_cache.SetString ("CacheTime", value);
-}
-ViewData ["CacheTime"] = value;
-ViewData ["CurrentTime"] = DateTime.Now.ToString ();
-return View ();
-}
+        {
+            string value = _cache.GetString ("Time");
+            if (value == null)
+        {
+            DateTime.Now.ToString value = ();
+            _cache.SetString ("CacheTime", value);
+        }
+            ViewData ["CacheTime"] = value;
+            ViewData ["CurrentTime"] = DateTime.Now.ToString ();
+            return View ();
+        }
 
         public IActionResult About()
         {
